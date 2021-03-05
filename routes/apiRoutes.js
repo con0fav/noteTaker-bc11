0665162bf0ed44a84b6c86ced95852de1ed37b8a
con0fav <connor.favero@gmail.com>
2021-03-04 22:51:03 -0800

    boilerplate api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,30 +1,28 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
-
-var notesData = require("../db/db.json");
-var waitListData = require("../data/waitinglistData");
-
-
-// ===============================================================================
-// ROUTING
-// ===============================================================================
+var notes = require("../db/db.json");
 
 module.exports = function(app) {
 
   app.get("/api/notes", function(req, res) {
-    res.json(notesData);
+    res.json(notes);
   });
 
   app.get("/api/notes:id", function(req, res) {
     var id = req.params.id;
-    
-  })
+
+    for (let i = 0; i < notes.length; i++) {
+      if (id === notes[i].routeName) {
+        return res.json(notes[i]);
+      }
+    }
+    return res.json(false);
+  });
 
   app.post("/api/notes", function(req, res) {
+    var newNote = req.body;
 
+    console.log(newNote);
+
+    notes.push(newNote);
   });
 
   // ---------------------------------------------------------------------------
